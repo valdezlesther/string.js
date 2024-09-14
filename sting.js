7549790ab34802d7492fdcc7d56097b60e94b9e9
valdezlesther <marklesthervaldez52@gmail.com>
2024-09-14 12:29:30 +0800

    Update sting.js

--- sting.js
+++ sting.js
@@ -3,6 +3,6 @@ leg favActorLastName = "Freeman";
 let fullName = favActorFirstName + "" + favActorLastName;
 let uppercase = fullname.toUpperCase();
 let lowercase = "Britney".toLowerCase();
-let message = "My favorite actor is" + uppercase +, His best show is Fast and Furious.";
+let message = "My favorite actor is" + uppercase +, His best show is Gunner.";
 
 console.log(message);
